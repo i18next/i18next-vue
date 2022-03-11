a4a48847ff2dfbb0fa7d5e7bd5db6d799f8d23b8
Konrad Kügler <swamblumat-git@yahoo.de>
2022-03-12 00:07:57 +0100

    Add i18nOptions support and re-render on changes

--- index.ts
+++ index.ts
@@ -1,31 +1,55 @@
 import _Vue from "vue";
-import { i18n, TFunction } from "i18next";
+import { i18n, TFunction, TOptions } from "i18next";
 
 declare module "vue/types/vue" {
     interface Vue {
         $t: TFunction;
+        $i18next: i18n;
 
         __bundles?: Array<[string, string]>;  // the bundles loaded by the component
-        __key?: (key: string) => string; // local to each component with an <i18n> block
+        __translate: (key: string, options?: TOptions | string) => string; // local to each component with an <i18n> block or i18nOptions
     }
 }
 
+
+type Messages = { [index: number]: string | Messages };
 declare module "vue/types/options" {
-    interface ComponentOptions<V extends Vue> {
+    interface ComponentOptions<V extends _Vue, Data, Methods, Computed, PropsDef, Props> {
         __i18n?: string[]; // due to package @intlify/vue-i18n-loader, each component with at least one <i18n> block has __i18n set
+        i18nOptions?: {
+            lng?: string;
+            keyPrefix?: string;
+            namespaces?: string | string[];
+            messages?: Messages;
+        }
     }
 }
 
 interface VueI18NextOptions {
     i18next: i18n;
+    rerenderOn?: ('languageChanged' | 'loaded' | 'added' | 'removed' | 'loaded')[];
 }
 
-export default function install(Vue: typeof _Vue, { i18next }: VueI18NextOptions): void {
+export default function install(Vue: typeof _Vue, {
+    i18next,
+    rerenderOn = ['languageChanged', 'loaded', 'added', 'removed'],
+}: VueI18NextOptions): void {
+    const genericT = i18next.t.bind(i18next);
+    // the observable (internally) tracks which Vue instances use translations and will automatically 
+    // trigger re-renders by Vue when the value of 'lastI18nChange' changes
+    const changeTracker = Vue.observable({ lastI18nChange: new Date() });
+    const invalidate: () => void = () => changeTracker.lastI18nChange = new Date();
+    const usingTranslation: () => void = () => changeTracker.lastI18nChange;
+    rerenderOn.forEach(event => i18next.on(event, invalidate))
+
     Vue.mixin({
         beforeCreate() {
-            this.__key = undefined; // required to enable proxied access to `__key` in the $t function
-
-            if (!this.$options.__i18n) return;
+            const options = this.$options;
+            if (!options.__i18n && !options.i18nOptions) {
+                //FIXME: oder muss man es in dem Fall gar nicht zuweisen
+                this.__translate = genericT;
+                return;
+            }
 
             // each component gets its own 8-digit random namespace prefixed with its name if available
             const name = this.$options.name;
@@ -34,28 +58,85 @@ export default function install(Vue: typeof _Vue, { i18next }: VueI18NextOptions
 
             // used to store added resource bundle identifiers for later removal upen component destruction
             this.__bundles = [];
-
-            // iterate all <i18n> blocks' contents as provided by @intlify/vue-i18n-loader and make them available to i18next
-            this.$options.__i18n.forEach(bundle => {
-                Object.entries(JSON.parse(bundle)).forEach(([lng, resources]) => {
+            const loadBundle = (bundle: Messages) => {
+                Object.entries(bundle).forEach(([lng, resources]) => {
                     i18next.addResourceBundle(lng, localNs, resources, true, false);
                     this.__bundles!.push([lng, localNs]);
                 });
+            }
+
+            // iterate all <i18n> blocks' contents as provided by @intlify/vue-i18n-loader and make them available to i18next
+            options.__i18n?.forEach(bundle => {
+                loadBundle(JSON.parse(bundle));
             });
 
-            // transforms a non-namespaced key into a component-scoped key
-            this.__key = (key: string) => {
-                const nsSeparator = i18next.options.nsSeparator;
-                const includesNs = typeof nsSeparator === "string" && key.includes(nsSeparator);
-                return includesNs ? key : `${localNs}:${key}`; // prefix the key with its component's local namespace if no namespace is specified
+            let { lng, ns, keyPrefix } = handleI18nOptions(options, loadBundle);
+            if (this.__bundles?.length) { // has local translations
+                ns = [localNs].concat(ns ?? []); // add component-local namespace, thus finding and preferring local translations
+            }
+
+            // Translation function respecting lng and ns. The namespace can be overriden in $t calls using a key prefix or the 'ns' option.
+            const t = getTranslationFunction(lng, ns);
+            this.__translate = (key: string, options?: TOptions | string): string => {
+                if (!keyPrefix || includesNs(key)) {
+                    return t(key, options);
+                } else { // adding keyPrefix only if key is not namespaced
+                    return t(keyPrefix + key, options);
+                }
             };
         },
         destroyed(this: _Vue) {
+            //FIXME: das ist doch Quatsch, weil es auf Instanzebene passiert und nicht, wenn die letzte Instanz verschwindet -> reference counting - per component???
+            // andererseits ist die Registrierung auch pro Instanz (was unnötig ist)
             this.__bundles?.forEach(([lng, ns]) => i18next.removeResourceBundle(lng, ns)); // avoid memory leaks
         }
     });
 
-    Vue.prototype.$t = function (this: _Vue, key: string, options?: string | Record<string, any>): string {
-        return i18next.t(this.__key?.(key) ?? key, options);
+    Vue.prototype.$t = function (this: _Vue, key: string, options?: TOptions | string): string {
+        usingTranslation(); // called during render, so we will get re-rendered when translations change
+        return this.__translate(key, options);
     };
+    Vue.prototype.$i18next = i18next;
+
+    function getTranslationFunction(lng?: string, ns?: string[]): TFunction {
+        if (lng) {
+            return i18next.getFixedT(lng, ns);
+        } else if (ns) {
+            return i18next.getFixedT(null, ns);
+        } else {
+            return genericT;
+        }
+    }
+
+    function includesNs(key: string): boolean {
+        const nsSeparator = i18next.options.nsSeparator;
+        return typeof nsSeparator === "string" && key.includes(nsSeparator);
+    }
+
+    function handleI18nOptions(options: any, loadBundle: (bundle: Messages) => void) {
+        let lng: string | undefined;
+        let ns: string[] | undefined;
+        let keyPrefix: string | undefined;
+        if (options.i18nOptions) {
+            let messages: Messages | undefined;
+            let namespaces: string | string[] | undefined;
+            ({
+                lng,
+                namespaces = i18next.options.defaultNS,
+                keyPrefix,
+                messages,
+            } = options.i18nOptions);
+
+            // make i18nOptions.messages available to i18next
+            if (messages) {
+                loadBundle(messages);
+            }
+
+            ns = typeof namespaces === 'string' ? [namespaces] : namespaces;
+            if (ns) {
+                i18next.loadNamespaces(ns); // load configured namespaces
+            }
+        }
+        return { lng, ns, keyPrefix };
+    }
 }
