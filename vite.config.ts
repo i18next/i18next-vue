b2b4a0f79ce11ffbfc458cdc577046c83e416fa8
Konrad Kügler <swamblumat-git@yahoo.de>
2024-08-20 22:39:19 +0200

    build: Update dependencies & prepare 5.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
 		globals: true,
 		environment: "jsdom",
 		coverage: {
-			reporter: "text",
+			reporter: ["text"],
 		},
 	},
 });
