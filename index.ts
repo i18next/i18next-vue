4c7e769660595c74e00e06dc01fe3520dfb0b04d
Alexander Chepurnoy <cany748@gmail.com>
2025-02-12 08:02:08 +0000

    fix: types in $t

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { i18n, TFunction, Namespace, KeyPrefix } from "i18next";
 
 declare module "vue" {
 	interface ComponentCustomProperties {
-		$t: TFunction;
+		$t: TFunction<Namespace>;
 		$i18next: i18n;
 	}
 }
